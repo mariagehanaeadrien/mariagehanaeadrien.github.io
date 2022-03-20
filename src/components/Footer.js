289e7157856b0f7a15b4180315994f3e925be3b6
AdrienBenamira <benamira.adrien@gmail.com>
2022-03-20 16:08:54 +0800

    deleate info page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,12 +17,12 @@ function Footer() {
 
           <small className='website-rights_Hanae'>
           <p> Mariage Adrien & Hanae </p>
-          <p>  15 juillet 2023 </p>
+          <p>  XXX </p>
           </small>
 
           <div className='footer-link-items'>
-            <small className='website-rights'> <p> Hanae : +65 8894 1425  </p> <p> Adrien : +65 8921 7967 </p> </small>
-            <small className='website-rights'> <p> 10 Jurong Lake Link </p> <p> #02-37 Lake Grande Condominum </p>  648131 Singapore</small>
+            <small className='website-rights'> <p> Hanae : XXX  </p> <p> Adrien : XXX </p> </small>
+            <small className='website-rights'> <p> XXX </p> <p> XXX </p>  XXX </small>
             <Link to='#' onClick={(e) => {
                     window.location.href = "mailto:contact@adressecommune.ovh";
                     e.preventDefault();}}> contact@adressecommune.ovh </Link>
